4f4ddc58d5ddfefde9a4060005a3a3dd8684dba9
Hans <ccoccc14@gmail.com>
2023-11-14 03:46:44 +0000

    1114第二

--- 高中/2.py
+++ 高中/2.py
@@ -1,4 +1,30 @@
 A=[]
+A.append(float(input("身高(cm): ")))
+A.append(float(input("體重(kg): ")))
+
+A.append(A[1]/((A[0]/100)**2))
+
+print(f"\n身高(cm):{A[0]} 體重(kg):{A[1]} BMI:{A[2]}")
+print(A)
+
+
+#2
+"""
+A=[0,0,0]
+A[0]=int(float("身高(cm): "))
+A[1]=int(float("體重(kg): "))
+A[2]=A[1]/((A[0]/100)**2)
+
+print(A[0])
+print(A[1])
+print(A[2])
+"""
+
+
+
+#1
+"""
+A=[]
 A.append(int(input("底: ")))
 A.append(int(input("高: ")))
 
@@ -7,10 +33,8 @@ A.append((A[0]*A[1])/2)
 print(f"\n底:{A[0]} 高:{A[1]} 面積:{A[2]}")
 print(A)
 
+#
 
-
-#正常ㄉ方法
-"""
 A=[0,0,0]
 A[0]=int(input("底: "))
 A[1]=int(input("高: "))
